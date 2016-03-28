b898fcc15e20d4c95401d913aefcb91502a4152b
Jirawong Wongdokpuang <jirawong.wdp@gmail.com>
2016-03-28 19:28:14 +0700

    add MOdel

--- src/main/java/com/example/springproject/service/IndexService.java
+++ src/main/java/com/example/springproject/service/IndexService.java
@@ -1,11 +1,29 @@
 package com.example.springproject.service;
 
+import java.util.List;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.example.springproject.model.User;
+import com.example.springproject.repository.UserRepository;
 
 @Service
 public class IndexService {
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	public List<User> addUser(User user){
+		return userRepository.add(user);
+	}
+	
+	public User appendZ(User user){
+		String firstName = user.getFirstName() + "Z";
+		user.setFirstName(firstName);
+		return user;
+	}
 
 	public Map<String, String> appendZ(Map<String, String> map){
 		
@@ -16,6 +34,10 @@ public class IndexService {
 		
 		
 		return map;
+	}
+
+	public List<User> getUser() {
+		return userRepository.getAll();
 	}
 	
 }
